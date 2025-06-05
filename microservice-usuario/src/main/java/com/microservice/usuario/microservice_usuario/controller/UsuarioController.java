25c5b6cc677c2d15bac4a16c4ba0381a8a2e91e4
Héctor <he.robledo@duocuc.cl>
2025-06-05 15:25:20 -0400

    Errores arreglados

--- microservice-usuario/src/main/java/com/microservice/usuario/microservice_usuario/controller/UsuarioController.java
+++ microservice-usuario/src/main/java/com/microservice/usuario/microservice_usuario/controller/UsuarioController.java
@@ -15,11 +15,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.microservice.usuario.microservice_usuario.dto.UsuarioDTO;
 import com.microservice.usuario.microservice_usuario.model.Usuario;
 import com.microservice.usuario.microservice_usuario.service.UsuarioService;
 
@@ -46,34 +48,56 @@ public class UsuarioController {
         Optional<Usuario> usuario = usuarioService.getUserById(id_usuario);
 
         if(usuario.isPresent()){
+            
+            UsuarioDTO dto = new UsuarioDTO();
+            dto.setId_usuario(usuario.get().getId_usuario());
+            dto.setRut(usuario.get().getRut());
+            dto.setNombres(usuario.get().getNombres());
+            dto.setApellidos(usuario.get().getApellidos());
+            dto.setCorreo(usuario.get().getCorreo());
+
             return ResponseEntity.ok()
                         .header("mi-encabezado","valor")
                         .body(usuario.get());
+
         }else{
             Map<String,String> errorBody = new HashMap<>();
             errorBody.put("message","No se encontró el usuario con ese ID: " + id_usuario);
             errorBody.put("status","404");
             errorBody.put("timestamp",LocalDateTime.now().toString());
+            
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(errorBody);
         }
 
     }
 
-    @PostMapping("/save")
-    public ResponseEntity<?> save(@Valid @RequestBody Usuario usuario) {
+    @PostMapping
+    public ResponseEntity<?> save(@Valid @RequestBody UsuarioDTO usuarioDTO) {
         try{
 
+            Usuario usuario = new Usuario();
+            usuario.setRut(usuarioDTO.getRut());
+            usuario.setNombres(usuarioDTO.getNombres());
+            usuario.setApellidos(usuarioDTO.getApellidos());
+            usuario.setCorreo(usuarioDTO.getCorreo());
+
             Usuario usuarioGuardado = usuarioService.save(usuario);
+
+            UsuarioDTO responseDTO = new UsuarioDTO();
+            responseDTO.setId_usuario(usuarioGuardado.getId_usuario());
+            responseDTO.setRut(usuarioGuardado.getRut());
+            responseDTO.setNombres(usuarioGuardado.getNombres());
+            responseDTO.setApellidos(usuarioGuardado.getApellidos());
+            responseDTO.setCorreo(usuarioGuardado.getCorreo());
 
             URI location = ServletUriComponentsBuilder
                     .fromCurrentRequest()
                     .path("/{id}")
                     .buildAndExpand(usuarioGuardado.getId_usuario())
                     .toUri();
-            return ResponseEntity
-                    .created(location)
-                    .body(usuarioGuardado);
+
+            return ResponseEntity.created(location).body(responseDTO);
 
         }catch(DataIntegrityViolationException e){
             //Da error si hay un campo único duplicado
@@ -85,19 +109,27 @@ public class UsuarioController {
     
 
     //localhost:8090/api/v1/usuarios/{id_usuario}
-    @PostMapping("{id_usuario}")
-    public ResponseEntity<Usuario> update(@PathVariable int id_usuario, @RequestBody Usuario usuario) {
+    @PutMapping("{id_usuario}")
+    public ResponseEntity<UsuarioDTO> update(@PathVariable int id_usuario, @RequestBody UsuarioDTO usuarioDTO) {
         try {
             
-            Usuario usu = usuarioService.getUserById2(id_usuario);
-            usu.setId_usuario(id_usuario);
-            usu.setRut(usuario.getRut());
-            usu.setNombres(usuario.getNombres());
-            usu.setApellidos(usuario.getApellidos());
-            usu.setCorreo(usuario.getCorreo());
+            Usuario usuario = new Usuario();
+            usuario.setId_usuario(id_usuario);
+            usuario.setRut(usuarioDTO.getRut());
+            usuario.setNombres(usuarioDTO.getNombres());
+            usuario.setApellidos(usuarioDTO.getApellidos());
+            usuario.setCorreo(usuarioDTO.getCorreo());
 
-            usuarioService.save(usuario);
-            return ResponseEntity.ok(usuario);
+            Usuario usuarioActualizado = usuarioService.save(usuario);
+        
+            UsuarioDTO responseDTO = new UsuarioDTO();
+            responseDTO.setId_usuario(usuarioActualizado.getId_usuario());
+            responseDTO.setRut(usuarioActualizado.getRut());
+            responseDTO.setNombres(usuarioActualizado.getNombres());
+            responseDTO.setApellidos(usuarioActualizado.getApellidos());
+            responseDTO.setCorreo(usuarioActualizado.getCorreo());
+            
+            return ResponseEntity.ok(responseDTO);
 
 
         } catch (Exception ex) {
